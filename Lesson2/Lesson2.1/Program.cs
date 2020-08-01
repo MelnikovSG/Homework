d3da9712f6daac138e63526031388c6763265224
MelnikovSG <57829903+MelnikovSG@users.noreply.github.com>
2020-08-01 13:58:55 +0700

    Урок 2

---
+++ Lesson2/Lesson2.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2._1
+{
+    class Program
+    {
+        static int CountN()
+        {
+            /* Написать метод подсчета количества цифр числа.
+               Мельников Сергей
+            */
+
+            int n;
+
+            Console.WriteLine("Введите число");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+
+            while (n != 0)
+            {
+                n = n/10;
+                count++;
+                
+            }
+            return count;
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Количество цифр в числе равно {CountN()}");
+            Console.ReadLine();
+        }
+    }
+}
